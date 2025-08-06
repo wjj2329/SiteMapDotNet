ccfa3297240498046be2dd927337e51e47475a15
William Jones <william.jones@allata.com>
2025-08-05 18:50:26 -0600

    Initial Commit

---
+++ SiteMapNodeNetCore/SiteMap.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiteMapNodeNetCore
+{
+    public class SiteMap
+    {
+        private SiteMapNode _rootNode;
+        public SiteMapNode RootNode { get { return _rootNode; } } 
+
+    }
+}
---
+++ SiteMapNodeNetCore/SiteMapNode.cs
@@ -0,0 +1,20 @@
+﻿namespace SiteMapNodeNetCore
+{
+    public class SiteMapNode
+    {
+        public SiteMapNode RootNode; //pointer to parrent rootnode
+        public string Title;
+        public List<SiteMapNode> Children;
+        public List<SiteMapNode> Parent;
+        public string Description;
+        public bool HasDescription;
+        public string Key;
+        public SiteMapNode NextNode;
+
+        public bool Equals(SiteMapNode other)
+        {
+            return this == other;
+        }
+
+    }
+}
